a237f3f44474596c28de0768bc6a482499d5f926
emers0mn <emeron0000@gmail.com>
2023-02-23 13:34:32 -0300

    Utilizando POST

--- routing/app.js
+++ routing/app.js
@@ -5,24 +5,35 @@ const servidor = http.createServer((req, res) =>{
     /// Primeiro temos que extrair o método ex:
 
     if(req.method === 'GET'){
-        return pedido_URL(req, res);
-    } else {
-        return console.log(`O método usado foi ${req.method}, por isso pode ter ocorrido algo tipo de erro`);
+        return pedido_GET(req, res);
+
+    } else if(req.method === 'POST') {
+        return pedido_POST(req, res);
+    } else{ 
+        res.statusCode = 501;
+        res.end(`O método não pode ser utilizado pelo serviro ${req.method}`)
     };
 });
 
-function pedido_URL(req, res){ 
+function pedido_GET(req, res){ 
     const path = req.url;
 
     if(path === '/') {
         res.statusCode = 200;
-        res.end('Bem vindos ao meu primeiro servidor e API criado com Node.js')
+        return res.end('Bem vindos ao meu primeiro servidor e API criado com Node.js')
     } else if(path === '/receitas') {
         res.statusCode = 200;
         res.end(JSON.stringify(receitas))
     } else {
         res.statusCode = 404;
         res.end('Pagina nao encontrada')
+    }
+}
+
+function pedido_POST(req, res){
+    if(req.url === '/receitas/bolo'){
+        res.statusCode = 200;
+        return res.end('O servidor recebeu uma solicitação POST /receitas/bolos')
     }
 }
 
